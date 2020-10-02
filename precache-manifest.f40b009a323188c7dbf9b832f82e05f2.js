3bd11df15fbbba0d6e7f03fbfd6c16ac17036093
Vitalii-Zaharchuk <vitaliy.zakharchuk19@gmail.com>
2020-10-02 18:46:55 +0300

    Updates

---
+++ precache-manifest.f40b009a323188c7dbf9b832f82e05f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f26f0dbcea5d7dd19f882fd742d0a1",
+    "url": "/Lk/index.html"
+  },
+  {
+    "revision": "4ebfd58f9d4749051f05",
+    "url": "/Lk/static/css/3.a0871ffc.chunk.css"
+  },
+  {
+    "revision": "43741f8fe6904143ac07",
+    "url": "/Lk/static/css/4.75b5e9aa.chunk.css"
+  },
+  {
+    "revision": "7dabf75522b0ba747769",
+    "url": "/Lk/static/css/5.a5fa6e5e.chunk.css"
+  },
+  {
+    "revision": "bbd48845f6832b037b43",
+    "url": "/Lk/static/css/main.d1355195.chunk.css"
+  },
+  {
+    "revision": "e3bb9b55b481f79a10ff",
+    "url": "/Lk/static/js/2.989658fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Lk/static/js/2.989658fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebfd58f9d4749051f05",
+    "url": "/Lk/static/js/3.3b3a1217.chunk.js"
+  },
+  {
+    "revision": "43741f8fe6904143ac07",
+    "url": "/Lk/static/js/4.f4b9d400.chunk.js"
+  },
+  {
+    "revision": "7dabf75522b0ba747769",
+    "url": "/Lk/static/js/5.a47521ac.chunk.js"
+  },
+  {
+    "revision": "bbd48845f6832b037b43",
+    "url": "/Lk/static/js/main.099bae9a.chunk.js"
+  },
+  {
+    "revision": "e76510ff440d1168086d",
+    "url": "/Lk/static/js/runtime-main.f37259bd.js"
+  }
+]);
